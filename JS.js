008a3477e7eb612ac50f6e1e2ea87a41e93ccc23
emerson2017 <emersondima@hotmail.com>
2019-01-15 18:13:12 -0300

    manada

---
+++ JS.js
@@ -0,0 +1,17 @@
+function mudaRed(){
+	var red = document.getElementById("Get");
+	red.value="Red";
+	red.style.color="red";
+
+}
+function mudaBlue(){
+	var blue = document.getElementById("Get");
+	blue.value="Blue";
+	blue.style.color="blue";
+}
+function mudaGreen(){
+	var green = document.getElementById("Get");
+	green.value="Green";
+	green.style.color="green";
+}
+
